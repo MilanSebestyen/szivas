312e26652259733e97bbeed857e8f33378078554
sebestyen.milan <sebestyen.milan19k@egressy.info>
2024-12-03 13:57:10 +0100

    Add project files.

---
+++ szívás/KecskeClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace szivas
+{
+    class KecskeClass
+    {
+        public int id { get; set; }
+        public int kor { get; set; }
+        public int suly { get; set; }
+        public int magassag { get; set; }
+        public string neme { get; set; }
+        public string nev { get; set; }
+    }
+}
